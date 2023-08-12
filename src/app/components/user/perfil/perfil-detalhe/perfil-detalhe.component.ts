919f2225a4fa29b6eaad438b6eb3617edfb069e0
Frederico Rook <fredericorook@hotmail.com>
2023-08-12 14:12:38 -0300

    .

--- src/app/components/user/perfil/perfil-detalhe/perfil-detalhe.component.ts
+++ src/app/components/user/perfil/perfil-detalhe/perfil-detalhe.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { ValidatorField } from '@app/helpers/ValidatorField';
 import { UserUpdate } from '@app/models/identity/UserUpdate';
@@ -11,7 +16,7 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-perfil-detalhe',
   templateUrl: './perfil-detalhe.component.html',
-  styleUrls: ['./perfil-detalhe.component.scss']
+  styleUrls: ['./perfil-detalhe.component.scss'],
 })
 export class PerfilDetalheComponent implements OnInit {
   @Output() changeFormValue = new EventEmitter();
@@ -19,13 +24,14 @@ export class PerfilDetalheComponent implements OnInit {
   userUpdate = {} as UserUpdate;
   form!: FormGroup;
 
-  constructor( private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     public accountService: AccountService,
     public palestranteService: PalestranteService,
     private router: Router,
     private toaster: ToastrService,
     private spinner: NgxSpinnerService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.validation();
@@ -34,28 +40,29 @@ export class PerfilDetalheComponent implements OnInit {
   }
 
   private verificaForm(): void {
-    this.form.valueChanges
-      .subscribe(() => this.changeFormValue.emit({...this.form.value}))
+    this.form.valueChanges.subscribe(() =>
+      this.changeFormValue.emit({ ...this.form.value })
+    );
   }
 
   private carregarUsuario(): void {
     this.spinner.show();
-    this.accountService
-      .getUser()
-      .subscribe(
-        (userRetorno: UserUpdate) => {
-          console.log(userRetorno);
-          this.userUpdate = userRetorno;
-          this.form.patchValue(this.userUpdate);
-          this.toaster.success('Usuário Carregado', 'Sucesso');
-        },
-        (error) => {
-          console.error(error);
-          this.toaster.error('Usuário não Carregado', 'Erro');
-          this.router.navigate(['/dashboard']);
-        }
-      )
-      .add(this.spinner.hide());
+    this.accountService.getUser().subscribe(
+      (userRetorno: UserUpdate) => {
+        console.log(userRetorno);
+        this.userUpdate = userRetorno;
+        this.form.patchValue(this.userUpdate);
+        this.toaster.success('Usuário Carregado', 'Sucesso');
+      },
+      (error) => {
+        console.error(error);
+        this.toaster.error('Usuário não Carregado', 'Erro');
+        this.router.navigate(['/dashboard']);
+      },
+      () => {
+        this.spinner.hide();
+      }
+    );
   }
 
   private validation(): void {
@@ -98,27 +105,29 @@ export class PerfilDetalheComponent implements OnInit {
       this.palestranteService.post().subscribe(
         () => this.toaster.success('Função palestrante Ativada!', 'Sucesso!'),
         (error) => {
-          this.toaster.error('A função palestrante não pode ser Ativada', 'Error');
+          this.toaster.error(
+            'A função palestrante não pode ser Ativada',
+            'Error'
+          );
           console.error(error);
         }
-      )
+      );
     }
 
-    this.accountService
-      .updateUser(this.userUpdate)
-      .subscribe(
-        () => this.toaster.success('Usuário atualizado!', 'Sucesso'),
-        (error) => {
-          this.toaster.error(error.error);
-          console.error(error);
-        }
-      )
-      .add(() => this.spinner.hide());
+    this.accountService.updateUser(this.userUpdate).subscribe(
+      () => this.toaster.success('Usuário atualizado!', 'Sucesso'),
+      (error) => {
+        this.toaster.error(error.error);
+        console.error(error);
+      },
+      () => {
+        this.spinner.hide();
+      }
+    );
   }
-
+  
   public resetForm(event: any): void {
     event.preventDefault();
     this.form.reset();
   }
-
 }
